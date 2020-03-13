cfee89a87dd307786220ca9fdf7c35c61a8af39a
ftanuma <ftanuma@gmail.com>
2020-03-12 18:06:21 -0700

    Context API testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import "./App.css";
 
-function App() {
+const ThemeContext = React.createContext({
+  mode: "light",
+  changeMode: () => {}
+});
+
+function Toolbar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ContextConsumingClass />
+      <ContextConsumingFuncMemo />
+    </>
   );
+}
+
+const ToolbarMemo = React.memo(Toolbar, (prevProps, nextProps) => {
+  return true; // Always avoid re-render
+});
+
+class ContextConsumingClass extends React.Component {
+  static contextType = ThemeContext;
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    console.log("ContextConsumingClass:render()");
+    return <p>ContextConsumingClass: I'm actually not using context :)</p>;
+  }
+}
+
+function ContextConsumingFunc() {
+  const context = useContext(ThemeContext);
+
+  console.log("ContextConsumingFunc:render()");
+  return <p>ContextConsumingFunc: I'm actually not using context :)</p>;
+}
+
+const ContextConsumingFuncMemo = React.memo(ContextConsumingFunc, (prevProps, nextProps) => {
+  return true; // Always avoid re-render
+});
+
+class App extends React.Component {
+  state = {
+    mode: "dark"
+  };
+  changeMode = () => {
+    this.setState(prevState => {
+      if (this.state.mode === "dark") {
+        return { mode: "light" };
+      } else {
+        return { mode: "dark" };
+      }
+    });
+  };
+
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{ mode: this.state.mode, changeMode: this.changeMode }}
+      >
+        <div className="App">
+          <ToolbarMemo></ToolbarMemo>
+          <button onClick={this.changeMode}>change mode</button>
+          <p>{this.state.mode}</p>
+        </div>
+      </ThemeContext.Provider>
+    );
+  }
 }
 
 export default App;
